c89da268e6fa4379018d2ac56ff21d032cdcb0d3
awanaprilino <aprilinoawan@gmail.com>
2020-04-11 18:55:26 +0700

    first

---
+++ modul/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','ngekampu_modul');
+ define('PASS','awan modul 123');
+ define('DB','ngekampu_modul');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
